f08c5c37569011d5a330dda138f860b25c3b6f14
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-09-21 13:31:07 -0400

    Finish RNG

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,25 @@
-// add your imports here
+#include <iostream>
+#include <cstdint>
+#include <bitset>
+using namespace std;
+
 int main(){
-  // code here
+  unsigned int seed, num;
+  signed int min, max, value;
+  cin >> seed >> num >> min >> max;
+
+  for(int x =0; x < num; x++)
+  {
+    seed ^= seed << 13;
+    //cout << bitset<32>(seed) << endl;
+    seed ^= seed >> 17;
+    //cout << bitset<32>(seed) << endl;
+    seed ^= seed << 5;
+    //cout << bitset<32>(seed) << endl;
+
+
+    value = min + (seed  % (max - min + 1));
+    cout <<  value << endl;
+  }
+  return 0;
 }
