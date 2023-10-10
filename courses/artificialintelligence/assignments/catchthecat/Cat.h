1dcc1fc2b58bf1d2f46712057356234ae81af284
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-10-10 16:51:43 -0400

    CTC tests passing

--- courses/artificialintelligence/assignments/catchthecat/Cat.h
+++ courses/artificialintelligence/assignments/catchthecat/Cat.h
@@ -4,51 +4,59 @@
 
 struct Cat : public IAgent {
   std::pair<int,int> move(const std::vector<bool>& world, std::pair<int,int> catPos, int sideSize ) override{
-    return {0,0}; // todo: change this
+    std::vector<std::pair<int,int>> newPos;
+    int half = sideSize/2;
+    int px = (half + catPos.first);
+    int py = (half + catPos.second);
+    int north = (half + catPos.second - 1);
+    int south = (half + catPos.second + 1);
+    int east = (half + catPos.first + 1);
+    int west = (half + catPos.first - 1);
+
+    if(py % 2 == 1)
+    {
+      if(!world[(north)*sideSize+east])
+        newPos.emplace_back(catPos.first+1, catPos.second-1);
+        //newPos.emplace_back(east, north);
+      if(!world[(north)*sideSize+px])
+        newPos.emplace_back(catPos.first, catPos.second-1);
+        //newPos.emplace_back(px, north);
+      if(!world[(py)*sideSize+east])
+        newPos.emplace_back(catPos.first+1, catPos.second);
+        //newPos.emplace_back(east, py);
+      if(!world[(south)*sideSize+east])
+        newPos.emplace_back(catPos.first+1, catPos.second+1);
+        //newPos.emplace_back(east, south);
+      if(!world[(south)*sideSize+px])
+        newPos.emplace_back(catPos.first, catPos.second+1);
+        //newPos.emplace_back(px, south);
+      if(!world[(py)*sideSize+west])
+        newPos.emplace_back(catPos.first-1, catPos.second);
+        //newPos.emplace_back(west, py);
+    }
+    if(py % 2 == 0)
+    {
+      if(!world[(north)*sideSize+west])
+        newPos.emplace_back(catPos.first-1, catPos.second-1);
+        //newPos.emplace_back(west, north);
+      if(!world[(north)*sideSize+px])
+        newPos.emplace_back(catPos.first, catPos.second-1);
+        //newPos.emplace_back(px, north);
+      if(!world[(py)*sideSize+east])
+        newPos.emplace_back(catPos.first+1, catPos.second);
+        //newPos.emplace_back(east, py);
+      if(!world[(south)*sideSize+px])
+        newPos.emplace_back(catPos.first, catPos.second+1);
+        //newPos.emplace_back(px, south);
+      if(!world[(south)*sideSize+west])
+        newPos.emplace_back(catPos.first-1, catPos.second+1);
+        //newPos.emplace_back(west, south);
+      if(!world[(py)*sideSize+west])
+        newPos.emplace_back(catPos.first-1, catPos.second);
+        //newPos.emplace_back(west, py);
+    }
+    return newPos.back();
   }
 };
 
-std::pair<int,int> move(const std::vector<bool>& world, std::pair<int,int> catPos, int sideSize )
-{
-  std::pair<int,int> newPos;
-  int px = (sideSize + catPos.first) % sideSize;
-  int py = (sideSize + catPos.second) % sideSize;
-  int north = (sideSize + catPos.second) % sideSize;
-  int south = (sideSize + catPos.second) % sideSize;
-  int east = (sideSize + catPos.first + 1) % sideSize;
-  int west = (sideSize + catPos.first - 1) % sideSize;
-
-  if(py % 2 == 1)
-  {
-    if(world.at())
-    if(world.Get(Point2D(px, north)))
-      neighbors++;
-    if(world.Get(Point2D(east, north)))
-      neighbors++;
-    if(world.Get(Point2D(east, py)))
-      neighbors++;
-    if(world.Get(Point2D(east, south)))
-      neighbors++;
-    if(world.Get(Point2D(px, south)))
-      neighbors++;
-    if(world.Get(Point2D(west, py)))
-      neighbors++;
-  }
-  if(py % 2 == 0)
-  {
-    if(world.Get(Point2D(west, north)))
-      neighbors++;
-    if(world.Get(Point2D(px, north)))
-      neighbors++;
-    if(world.Get(Point2D(east, py)))
-      neighbors++;
-    if(world.Get(Point2D(px, south)))
-      neighbors++;
-    if(world.Get(Point2D(west, south)))
-      neighbors++;
-    if(world.Get(Point2D(west, py)))
-      neighbors++;
-  }
-  return newPos;
-}
 #endif
